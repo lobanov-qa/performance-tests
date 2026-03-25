9a67b1ca66112bbd802b01cc9f505f564fd26339
lobanov-qa <lobanov-qa@users.noreply.github.com>
2026-03-25 05:06:51 +0000

    deploy: f873b97668fd46491a678a1911d96ac1a7b28e8a

--- tools/config/locust.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class LocustUserConfig(BaseModel):
-    # Минимальное время ожидания между задачами (в секундах)
-    wait_time_min: float = 1
-
-    # Максимальное время ожидания между задачами (в секундах)
-    wait_time_max: float = 3
--- tools/routes.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class APIRoutes(StrEnum):
-    USERS = "/api/v1/users"
-    CARDS = "/api/v1/cards"
-    ACCOUNTS = "/api/v1/accounts"
-    DOCUMENTS = "/api/v1/documents"
-    OPERATIONS = "/api/v1/operations"
